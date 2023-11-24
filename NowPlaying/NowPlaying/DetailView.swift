0fc28d964c5e235e6a0eab46b8306533dd11a9af
Vicziand <113669347+Vicziand@users.noreply.github.com>
2023-11-24 16:00:29 +0100

    initial

---
+++ NowPlaying/NowPlaying/DetailView.swift
@@ -0,0 +1,27 @@
+
+import SwiftUI
+
+struct DetailView: View {
+    var m: Result
+    var body: some View {
+        Form {
+            Section {
+                Text(m.title ?? "")
+                    .font(.title)
+            }
+            Section {
+                AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(m.posterPath ?? "")")) { phase in
+                    phase.image?
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            Section {
+                Text(m.overview ?? "")
+            } header: {
+                Text("Overview")
+            }
+        }
+       
+    }
+}
